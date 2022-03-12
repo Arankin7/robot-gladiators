490b98014e0ee4ce4e3c778dd1763c84426866da
AnthonyR <arankin789@gmail.com>
2022-03-12 12:24:35 -0500

    Getting comfortable with variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert ("This is an alert!! Testing Javascript");
+// This creates a function named "fight"
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt ("What is your robot's name?");
+
+// window.alert (playerName);
+console.log (playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 +10);
+console.log("The robots name is " + playerName);
+// fight();
