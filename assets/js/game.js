982b6f28e62e8ff643756cecf9f84d73e69e7629
AnthonyR <arankin789@gmail.com>
2022-03-17 12:49:51 -0400

    High score funtional with localStorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -120,13 +120,31 @@ var startGame = function () {
 
 var endGame = function () {
 
+    window.alert("The game has now ended.  Let's see how you did!")
+
+    // Check localStorage for high score, if not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null){
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore){
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else{
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ".  Maybe next time!")
+    }
+
     // If player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert ("Great job, you've survived Robot Gladiators! You now have a score of " + playerInfo.money + ".");
-    }
-    else {
-        window.alert ("You've lose your robot in battle!");
-    }
+    // if (playerInfo.health > 0) {
+    //     window.alert ("Great job, you've survived Robot Gladiators! You now have a score of " + playerInfo.money + ".");
+    // }
+    // else {
+    //     window.alert ("You've lose your robot in battle!");
+    // }
 
     // Asking the player if they'd like to play again. 
     var playAgainConfirm = window.confirm("Would you like to play again?");
